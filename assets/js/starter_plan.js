8ef643b335d90e9718be7d74d0df4ab00b51c733
devfardin <contactfardin22@gmail.com>
2026-04-06 04:26:58 +0600

    create stain levels modal

--- assets/js/starter_plan.js
+++ assets/js/starter_plan.js
@@ -20,18 +20,23 @@ document.addEventListener('DOMContentLoaded', function () {
                             <div class="stater_plan__stain-level">
 
                                 <div class="stater_plan__body_title">
-                                    <span class="level-${index+1}">${stain.title}</span>
-                                    <span class="level-subtitle">${stain.subtitle}</span>
+                                    <span class="level level-${index + 1}">${stain.title}</span>
+                                    <span class="level_subtitle">${stain.subtitle}</span>
                                 </div>
 
                                 <div class="stater_plan__body_items">
                                     <ul>
-                                        ${stain.items.map(item => `<li>${item}</li>`).join('')}
+                                        ${stain.items.map(item => `<li>
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 15 15" fill="none">
+                                                 <path d="M5.62498 10.1062L3.01873 7.49999L2.13123 8.38124L5.62498 11.875L13.125 4.37499L12.2437 3.49374L5.62498 10.1062Z" fill="#1F3A5F"/>
+                                            </svg>
+                                            ${item}
+                                            </li>`).join('')}
                                     </ul>
                                     <p>${stain.note}</p>
                                 </div>
                             </div>`).join('')
-                        }
+            }
                 </div>
 
                 <div class="stater_plan__footer">
